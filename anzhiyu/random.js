557fe9779ede177dc4f27be77e1678ccb184c998
SunnyDay2333 <1946107707@qq.com>
2025-05-05 20:20:43 +0800

    Site updated: 2025-05-05 20:20:43

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/05/05/二分法-数的范围AcWing789/","2025/04/04/编译原理ex1-词法分析器/","2025/05/05/归并排序/","2025/05/05/快速排序/"];function toRandomPost(){
+var posts=["2025/05/05/二分法-数的范围AcWing789/","2025/05/05/快速排序/","2025/04/04/编译原理ex1-词法分析器/","2025/05/05/归并排序/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
