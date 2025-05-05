f186ef5f2df824781de8860ac43b888d15e07482
SunnyDay2333 <1946107707@qq.com>
2025-05-05 15:43:45 +0800

    Site updated: 2025-05-05 15:43:44

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/04/编译原理ex1-词法分析器/","2025/05/05/快速排序/"];function toRandomPost(){
+var posts=["2025/05/05/快速排序/","2025/04/04/编译原理ex1-词法分析器/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
